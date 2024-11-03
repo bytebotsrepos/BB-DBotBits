0d7da735cdaa26e9fb6ed711108b9ba4afb3b67b
ByteBots Repos <info@bytebots.net>
2024-11-03 16:30:18 +0000

    Update discordReverse.js
    Fixed the reverse as the second .split.reverse.join was not needed

--- discordReverse.js
+++ discordReverse.js
@@ -1,16 +1,16 @@
 /*
- _ __        _       ____        _       ____                      
-| __ ) _   _| |_ ___| __ )  ___ | |_ ___|  _ \ ___ _ __   ___  ___ 
-|  _ \| | | | __/ _ \  _ \ / _ \| __/ __| |_) / _ \ '_ \ / _ \/ __|
-| |_) | |_| | ||  __/ |_) | (_) | |_\__ \  _ <  __/ |_) | (_) \__ \
-|____/ \__, |\__\___|____/ \___/ \__|___/_| \_\___| .__/ \___/|___/
-       |___/                                      |_|              
-                    https://bytebots.net 
-Warning do not change anything in here unless you know what you are doing
-This is just a bit of fun i coded for testing it's quirky really but could 
-make your chat's a little more wierd as it reverses a scentence for example:
-hello become olleh
-*/              
+ * _ __        _       ____        _       ____
+ * | __ ) _   _| |_ ___| __ )  ___ | |_ ___|  _ \ ___ _ __   ___  ___
+ * |  _ \| | | | __/ _ \  _ \ / _ \| __/ __| |_) / _ \ '_ \ / _ \/ __|
+ * | |_) | |_| | ||  __/ |_) | (_) | |_\__ \  _ <  __/ |_) | (_) \__ \
+ * |____/ \__, |\__\___|____/ \___/ \__|___/_| \_\___| .__/ \___/|___/
+ *       |___/                                      |_|
+ *                    https://bytebots.net
+ * Warning do not change anything in here unless you know what you are doing
+ * This is just a bit of fun i coded for testing it's quirky really but could
+ * make your chat's a little more wierd as it reverses a scentence for example:
+ * hello become olleh
+ */
 
 const { client } = require('./discordHandler.js');
 
@@ -23,7 +23,7 @@ function discordReverse() {
         if (message.content.toLowerCase().startsWith('!reverse')) {
             // Extract the reason after the command
             const reverse = message.content.slice('!reverse'.length).trim();
-            const reverseSTR = reverse.split('').reverse().join('').split(' ').reverse().join(' ')
+            const reverseSTR = reverse.split('').reverse().join('');
 
             if (reverse) {
                 // Send the the reverse string
